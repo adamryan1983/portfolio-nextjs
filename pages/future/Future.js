0b3c872727c2c6d52b3ae1bc01149b61c2de4732
Adam Ryan <adamryan1983@gmail.com>
2021-01-29 20:50:28 -0330

    initial commit

---
+++ pages/future/Future.js
@@ -0,0 +1,10 @@
+import Layout from "../../components/Layout";
+import styles from './Future.module.scss'
+
+function Future() {
+  return (
+    <div className={styles.mainContainer}>
+      Future
+    </div>
+  )
+}
